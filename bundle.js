9e84755a727880ad0188fe2e3dd834d308fb0452
V-FEXrt <ash123125@yahoo.com>
2016-09-19 15:23:47 -0500

    class work

--- bundle.js
+++ bundle.js
@@ -12,6 +12,16 @@ var game = new Game(canvas, update, render);
 var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 var board = [];
 
+while(cards.length > 0) {
+  var index = Math.floor(Math.random() * (cards.length - 1));
+  board.push({card: cards[index], flip: false});
+  cards.splice(index, 1);
+}
+
+board.forEach(function(item){
+  console.log(item.card);
+});
+
 // TODO: Place the cards on the board in random order
 
 canvas.onclick = function(event) {
@@ -56,6 +66,38 @@ function render(elapsedTime, ctx) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // TODO: Render the board
+  board.forEach(function(item, idx){
+      if(item.flip){
+        renderCard(ctx, item, idx);
+      }else{
+        renderBack(ctx, item, idx);
+      }
+  });
+}
+
+function renderBack(ctx, item, idx){
+  var rect = getRect(idx);
+  //console.log(rect);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+  //console.log(rect);
+}
+
+function getRect(idx){
+  //console.log(idx);
+  //6 wide 3 tall
+  var width = Math.floor(canvas.width / 6);
+  var height = Math.floor(canvas.height / 3);
+  var x = idx - Math.floor(idx / 3);
+  var y = Math.floor(idx / 3);
+  return {
+    xt: x,
+    yt: y,
+    x: x * width,
+    y: y * height,
+    width: width - 5,
+    height: height -5
+  }
 }
 
 },{"./game":2}],2:[function(require,module,exports){
